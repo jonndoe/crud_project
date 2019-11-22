8cf91d08a9856f8c6c0084545a410a9d9fe482e3
user <user@mail.com>
2019-11-22 03:17:56 +0300

    ch5

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -127,3 +127,17 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-#from django.urls import reverse, resolve
+from django.urls import reverse, resolve
+
+from .views import SignupPageView
 
 
 # Create your tests here.
@@ -33,7 +35,7 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_superuser)
 
 
-'''
+
 
 class SignupTests(TestCase):
 
@@ -41,13 +43,14 @@ class SignupTests(TestCase):
     email = 'newuser@email.com'
 
     def setUp(self):
-        url = reverse('account_signup')
+        #url = reverse('account_signup')
+        url = reverse('signup')
         self.response = self.client.get(url)
 
 
     def test_signup_template(self):
         self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, 'account/signup.html')
+        self.assertTemplateUsed(self.response, 'signup.html')
         self.assertContains(self.response, 'Sign Up')
         self.assertNotContains(
             self.response, 'Hi there! I should not be on the page.')
@@ -62,5 +65,10 @@ class SignupTests(TestCase):
         self.assertEqual(get_user_model().objects.all()
                          [0].email, self.email)
 
+    def test_signup_view(self):  # new
+        view = resolve('/accounts/signup/')
 
-'''
+        self.assertEqual(
+            view.func.__name__,
+            SignupPageView.as_view().__name__
+        )
